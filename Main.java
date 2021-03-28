9daba9161f3c5d355897ff09f062681e6011fa90
Matthew Dean Brown <mattrewbrown@gmail.com>
2021-03-28 22:41:15 +0200

    Checking

---
+++ Main.java
@@ -0,0 +1,34 @@
+//Class nedded for java must also be the same name
+public class Main {
+
+//This wil execute the code
+    public static void main (String[] args) {
+//This will execute the line of code 'hello world'
+      System.out.println("Hello World");
+
+//stores and prints the name john
+      String name = "John ";
+      System.out.println(name);
+
+//stores number an prints value
+      byte myNum = 15;
+      System.out.println(myNum);
+
+//declaring variable late, assigning it a value and printing
+      byte firstNum = 0;
+      firstNum = 1;
+      System.out.println(firstNum);
+      System.out.println(name+ firstNum);
+      
+      String name1 = "Timmy ", name2 = "George ", name3 = "Carl";
+      System.out.println(name1+name2+name3);
+
+      byte one = 1, two = 2, three = 3;
+      System.out.println(one+two+three);
+
+      float deci = 6.75f;
+      double fract = 3.25d;
+      System.out.println(deci + fract);
+    }
+  }
+  
---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    public static void main() {
+        int num1 = 9;
+        double num2 = num1;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
